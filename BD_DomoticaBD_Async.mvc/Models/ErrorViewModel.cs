f107fc9f8009747b854fc5d9c2e5310f3b3bebe6
Tito160 <joeltito160@gmail.com>
2025-09-02 17:30:07 -0300

    mvc

---
+++ BD_DomoticaBD_Async.mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BD_DomoticaBD_Async.mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
